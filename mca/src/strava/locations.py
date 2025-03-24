e09bea7376c8974ed7396bb5dec97b0e284060f2
Per Morten  Halvorsen <per.morten.halvorsen@schibsted.com>
2025-03-24 11:59:34 +0100

    add fixed locations to explore

---
+++ mca/src/strava/locations.py
@@ -0,0 +1,89 @@
+"""
+This file contains typical cycling locations around Oslo,
+formatted as 'bounds' for the Strava API.
+
+Long/lat coordinates easily found via https://geojson.io/
+
+"""
+
+# west 
+filipstad = {
+    "name": "Filipstad",
+    "bounds": [
+        59.9050,
+        10.7086,
+        59.9111,
+        10.7225,
+    ]
+}
+
+bygdøy = {
+    "name": "Bygdøy",
+    "bounds": [
+        59.8944,
+        10.6663,
+        59.9197,
+        10.6927,
+    ]
+}
+
+holmenkollen = {
+    "name": "Holmenkollen",
+    "bounds": [
+        59.958,
+        10.636,
+        59.983,
+        10.666,
+    ]
+}
+
+tryvann = {
+    "name": "Tryvann",
+    "bounds": [
+        59.970,
+        10.665,
+        59.980,
+        10.675,
+    ]
+}
+
+# east
+barcode = {
+    "name": "Barcode",
+    "bounds": [
+        59.9069,
+        10.7485,
+        59.9072,
+        10.7634,
+    ]
+}
+
+ekeberg = {
+    "name": "Ekeberg",
+    "bounds": [
+        59.8819,
+        10.7545,
+        59.9008,
+        10.7899,
+    ]
+}
+
+groruddalen = {
+    "name": "Groruddalen",
+    "bounds": [
+        59.927,
+        10.800,
+        59.963,
+        10.9,
+    ]
+}
+
+e18 = {
+    "name": "E18",
+    "bounds": [
+        59.840,
+        10.761,
+        59.899,
+        10.779,
+    ]
+}
